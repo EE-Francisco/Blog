4a3df4221b5f30f46f09945808ef3067bb32f59d
Francisco <2420171077@estudiantesunibague.edu.co>
2023-02-05 15:35:23 -0500

    vista about

--- core/urls.py
+++ core/urls.py
@@ -3,10 +3,17 @@ from django.contrib import admin
 from django.urls import path
 from django.conf import settings
 
+from .views import HomeView, AboutView, ContactView
+
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('', HomeView.as_view(), name="home"),
+    path('about/', AboutView.as_view(), name="about"),
+    path('contact/', ContactView.as_view(), name="contact"),
 ]
 
 if settings.DEBUG:
-    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
-    urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
+    urlpatterns += static(settings.MEDIA_URL,
+                          document_root=settings.MEDIA_ROOT)
+    urlpatterns += static(settings.STATIC_URL,
+                          document_root=settings.STATIC_ROOT)
